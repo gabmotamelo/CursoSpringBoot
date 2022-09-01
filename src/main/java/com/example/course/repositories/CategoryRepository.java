ac77e03174cae60e5382e87cac03807444a9ce4d
Gabriel Mota <gabrielmota6@hotmail.com>
2022-09-01 15:40:30 -0300

    Product

--- src/main/java/com/example/course/repositories/CategoryRepository.java
+++ src/main/java/com/example/course/repositories/CategoryRepository.java
@@ -1,7 +1,6 @@
 package com.example.course.repositories;
 
 import com.example.course.entities.Category;
-import com.example.course.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
